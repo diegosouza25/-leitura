ba28a17c69a11362c900b6923c0277f666b1eb78
Diego Souza <diegoalunocetep25@gmail.com>
2025-10-22 17:24:23 +0000

    ok

---
+++ infra/migrations/1761151017728_criar-tabela-livro.js
@@ -0,0 +1,41 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    
+pgm.sql(`
+CREATE TABLE livro(
+    id VARCHAR(36) PRIMARY KEY,
+    numero paginas INTEGER.
+    autor VARCHAR(255),
+    titulo VARCHAR(100) NOT NULL,
+    genero VARCHAR(100) NOT NULL,
+    data publicacao DATE,
+    isbm TEXT,
+    resumo TEXT,
+    imagem_capa TEXT,
+    editora VARCHAR(255),
+    disponibilidade boolean default true,
+    tags TEXT,
+    preco NUMERIC(10. 2) default 0.0
+);  
+`)
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.sql(`
+    DROP TABLE livro;
+    `)
+};
