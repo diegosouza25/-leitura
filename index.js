12856c2b50d9f11c4a4d15bfd1cc11e19aa13be6
Diego Souza <diegoalunocetep25@gmail.com>
2025-11-05 16:46:45 +0000

    ok

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const livroRepository = require("./infra/repository/livro");
+const http = require("node:http");
 
-livroRepository
-  .consultarTodos()
-  .then(function (resultado){
-    console.log(resultado);
-  })
-  .catch(function (error){
-    console.log("Deu ruim", error);
-  })
+const server = http.createServer(function (req,  res){
+//res.writeHead(200, {"content"})
+ res.end("Ok bem vindo");
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, function (){
+  consele.log("Inicializando srvidor HTTP na porta" + port)
+});
